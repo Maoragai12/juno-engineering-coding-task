7c21b8d1528bb18f3c77b812fe81af121ba4a852
Maor Agai <67845335+Maoragai12@users.noreply.github.com>
2022-03-22 02:52:27 +0200

    Introduce ImageCarousel component

--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,55 @@
-import React from "react";
-import { fetchImageUrls } from "../api/index";
+import React, { useEffect, useState } from 'react';
+import { fetchImageUrls, fetchImage } from "../api/index";
 
-const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
+const ImageCarousel = () => {
+
+    const [imagesUrls, setImagesUrls] = useState([]);
+    const [imageIndex, setImageIndex] = useState(0);
+    const [image, setImage] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+          (async function fetchImages() {
+            const images = await fetchImageUrls();
+            setImagesUrls(images);
+            const image = await fetchImage(imageIndex);
+            setImage(image);
+          })();
+    }, [imageIndex]);
+
+    async function handleOnClickPrev(){
+        setIsLoading(true);
+        const prevIndex = imageIndex - 1;
+        if(prevIndex === -1) 
+            setImageIndex(imagesUrls.length - 1);
+        else 
+            setImageIndex(prevIndex);
+        const image = await fetchImage(imageIndex);
+        setIsLoading(false);
+        setImage(image);
+    }
+
+    async function handleOnClickNext(){
+        setIsLoading(true);
+        const nextIndex = imageIndex + 1;
+        if(nextIndex === imagesUrls.length) 
+            setImageIndex(0);
+        else 
+            setImageIndex(nextIndex);
+        const image = await fetchImage(imageIndex);
+        setIsLoading(false);
+        setImage(image);
+    }
+
+    return (
+        <div>
+            <button onClick={handleOnClickPrev}>PREV</button>
+            <div>
+                <img src={image} alt="slide" 
+                style={{display: "inline-flex", 'justify-content': "center", 'align-items': "center", height: "300px", width: '50%'}}/>
+            </div>
+            <button onClick={handleOnClickNext}>NEXT</button>
+        </div>
+    );
 };
 export default ImageCarousel;
